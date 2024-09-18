be62c0ea8f90dd825223b46d5fbad36dc338a483
Tayyib Gbondo <gbondotahib72@gmail.com>
2024-09-18 13:31:44 +0000

    Signup page completly modified

--- lib/screens/sing_up.dart
+++ lib/screens/sing_up.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:give_thanks/screens/signup1.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class MyWidget extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new IconButton(
+        // Use the MdiIcons class for the IconData
+        icon: new Icon(MdiIcons.sword),
+        onPressed: () {
+          print('Using the sword');
+        });
+  }
+}
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -17,156 +29,196 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue, // Set background color to blue
-      body: SingleChildScrollView(
-        child: Center(
+      body: SafeArea(
+        child: Container(
+          // height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              // Add your image widget here
-              Image.asset('images/logo2.png'),
-              const Text(
-                "Let's us change the \n    world together",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 46,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'One gratitude at a time',
-                style: TextStyle(
-                    color: goldColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 430,
-                height: 42,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Add your button 1 onPressed logic here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: Colors.white, // Text color
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Icon(Icons.google),
-                      SizedBox(width: 8), // Space between the icon and text
-                      Text(
-                        'Continue with Google',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ), // Text
-                    ],
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 430,
-                height: 42,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const SignUp1()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: Colors.white, // Text color
-                  ),
-                  child: const Text(
-                    'Create Account',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+              //wwraping the signup contents in a container
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  children: [
+                    //The logo
+                    Image.asset(
+                      'images/logo2.png',
+                      width: 250,
                     ),
-                  ), // Text
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 430,
-                height: 42,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Add your button 3 onPressed logic here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor:
-                        const Color.fromARGB(255, 11, 136, 239), // Text color
-                    minimumSize:
-                        const Size(200, 50), // Width and height of the button
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(30), // Rounded corners
-                      side: const BorderSide(
-                          color: Colors.white, width: 2), // White border
-                    ),
-                  ),
-                  child: const Text('Explore Give Thanks'), // Text
-                ),
-              ),
 
-              const SizedBox(height: 20),
-              const Text.rich(
-                TextSpan(
-                  text: 'By signing up, you agree to our ',
-                  style: const TextStyle(
-                      color: Color.fromARGB(255, 224, 220, 220)),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Terms',
+                    const Text(
+                      "Let's us change the world together",
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.white),
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
-                    const TextSpan(text: ' and '),
-                    TextSpan(
-                      text: 'Privacy',
+                    const SizedBox(height: 5),
+                    Text(
+                      'One gratitude at a time',
                       style: TextStyle(
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.white),
+                          color: goldColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
               ),
 
-              const SizedBox(height: 10),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              //This container contain the Continue With google provide and Create Account Button also and the Explpre and as Annonimus use button
+              Container(
+                child: Column(
                   children: [
-                    const Text(
-                      'Already have an account?',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 224, 220, 220)),
-                    ),
-                    TextButton(
+                    ElevatedButton(
                       onPressed: () {
-                        // Add your login onPressed logic here
+                        // Add your button 1 onPressed logic here
                       },
-                      child: const Text(
-                        ' Login',
-                        style: TextStyle(
-                            color: Colors.white,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.white),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white, // Text color
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 13),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              MdiIcons.google,
+                              color: Colors.red,
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ), // Space between the icon and text
+                            const Text(
+                              'Continue with Google',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ), // Text
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUp1()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: Colors.white, // Text color
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 13),
+                          child: Text(
+                            'Create Account',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ), // Text
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Add your button 3 onPressed logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: const Color.fromARGB(
+                              255, 11, 136, 239), // Text color
+                          minimumSize: const Size(
+                              200, 50), // Width and height of the button
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(30), // Rounded corners
+                            side: const BorderSide(
+                                color: Colors.white, width: 2), // White border
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 13),
+                          child: Text('Explore Give Thanks'),
+                        ), // Text
                       ),
                     ),
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 15,
-              )
+
+              Container(
+                child: Column(
+                  children: [
+                    const Text.rich(
+                      TextSpan(
+                        text: 'By signing up, you agree to our ',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 224, 220, 220)),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Terms',
+                            style: TextStyle(
+                                color: Colors.white,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.white),
+                          ),
+                          const TextSpan(text: ' and '),
+                          TextSpan(
+                            text: 'Privacy',
+                            style: TextStyle(
+                                color: Colors.white,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Already have an account?',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 224, 220, 220),
+                              fontSize: 17,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              // Add your login onPressed logic here
+                            },
+                            child: const Text(
+                              ' Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
